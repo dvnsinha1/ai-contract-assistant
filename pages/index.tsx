2d85bccd21454920564d6d221c2543117c367c8d
dvnsinha1 <dvnsinha1@gmail.com>
2025-01-28 01:24:03 +0530

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>AI Contract Assistant</title>
+        <meta name="description" content="AI Contract Assistant" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        {/* Your main content here */}
+      </main>
+    </>
+  )
+} 
